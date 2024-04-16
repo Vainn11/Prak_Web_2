79acf1ceac723ac862c1e6e6f94022d04e74680f
Vainn11 <115350079+Vainn11@users.noreply.github.com>
2024-04-16 19:20:19 +0800

    Add files via upload

---
+++ Modul 2/Prak204.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="">
+        Nilai : <input type="number" name="nilai"> <br>
+        <input type="submit" name="submit" value="Konversi">
+    </form>
+    <?php
+    if(isset($_POST['submit'])){
+        $nilai = $_POST['nilai'];
+
+        if ($nilai == 0) {
+            echo "<b>Hasil : Nol</b>";
+            return false;
+        }
+        elseif ($nilai >= 1 && $nilai < 10) {
+            echo "<b>Hasil : Satuan</b>";
+            return false;
+        }
+        elseif ($nilai >= 10 && $nilai < 100) {
+            echo "<b>Hasil : Puluhan</b>";
+            return false;
+        } 
+        elseif ($nilai >= 100 && $nilai < 1000) {
+            echo "<b>Hasil : Ratusan</b>";
+            return false;
+        }
+        else {
+            echo "<b>Anda Menginput Melebihi Limit Bilangan</b>";
+            return false;
+        }
+    }
+    ?>
+</body>
+</html>
